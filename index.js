d940d267226778c4d1348542f89735799ff75376
Jim Nilsson <jimnils@hotmail.com>
2019-06-05 15:40:41 +0200

    lambda

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const getRandomQuote = require('get-random-quote')
-const Koa = require('koa')
-const router = require('koa-router')()
 
-function main() {
-  const app = new Koa()
-  router.get('/', async ctx => {
-    ctx.body = await getRandomQuote()
-  })
-  app.use(router.routes())
-  app.listen(3000)
+module.exports = async (req, res) => {
+  const quote = await getRandomQuote()
+  res.end(quote)
 }
-
-main()
